c0e6ca0fa0c38023ead7fce1da5de1cac9e95e97
0bmxa <0bmxa@users.noreply.github.com>
2018-08-15 13:24:38 +0200

    First version

--- Tempo/ViewController.swift
+++ Tempo/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bpmLabel: UILabel!
+    let tapRecognizer = UITapGestureRecognizer()
+    let doubleTapRecognizer = UITapGestureRecognizer()
+    
+    private var taps: [TimeInterval] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.bpmLabel.text = "0"
+
+        self.tapRecognizer.numberOfTapsRequired = 1
+        self.tapRecognizer.numberOfTouchesRequired = 1
+        self.doubleTapRecognizer.numberOfTapsRequired    = 1
+        self.doubleTapRecognizer.numberOfTouchesRequired = 2
+        
+        self.view.addGestureRecognizer(self.tapRecognizer)
+        self.view.addGestureRecognizer(self.doubleTapRecognizer)
+        
+        self.tapRecognizer.addTarget(self, action: #selector(self.viewTapped))
+        self.doubleTapRecognizer.addTarget(self, action: #selector(self.viewDoubleTapped))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func viewTapped() {
+        let now = Date().timeIntervalSinceReferenceDate
+        self.taps.append(now)
+        
+        guard
+            self.taps.count > 2,
+            let start = self.taps.first,
+            let end = self.taps.last
+        else { return }
+        
+        let duration = end - start
+        let beats = self.taps.count - 1 // last tap just sets the end of the last beat
+        let timePerBeat = duration / Double(beats)
+        let beatsPerMinute = 60.0 / timePerBeat
+        
+        self.bpmLabel.text = String(format: "%.1f", beatsPerMinute)
     }
-
-
+    
+    @objc func viewDoubleTapped() {
+        self.taps.removeAll()
+        self.bpmLabel.text = "0"
+    }
 }
 
